967c2b16d082967691ac63d8f61890776b1c2dc2
avilive <ivanyukaleksandr@gmail.com>
2020-07-03 21:57:54 +0300

    initial commit

---
+++ ip_filter/ip_filter/src/main.cpp
@@ -0,0 +1,13 @@
+﻿// ip_filter.cpp : Defines the entry point for the application.
+//
+
+#include "ip_filter.h"
+#include "lib.h"
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	system("PAUSE");
+	return 0;
+}
