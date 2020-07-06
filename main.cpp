042b62e9551e294f1df8da3da301b0b6530f3825
avilive <ivanyukaleksandr@gmail.com>
2020-07-06 12:38:12 +0300

    gtest added

--- main.cpp
+++ main.cpp
@@ -1,15 +1,14 @@
 ﻿// ip_filter.cpp : Defines the entry point for the application.
 //
 
-#include "ip_filter.h"
 #include "lib.h"
-#include "build_number.h"
+
 using namespace std;
+
 
 int main()
 {	
-	cout << BUILD_NUMBER << endl;
-	cout << "Hello CMake. " << endl;
-	
+	cout << get_build_number() << endl;
+	cout << get_greeting() << endl;
 	return 0;
 }
